34622bc41fd95ba57fb37b957ed1b544301d0a16
Javier Veron <javier.veron@gmail.com>
2025-03-18 09:49:04 -0300

    Clase 1

---
+++ app/Clase1/Counter.js
@@ -0,0 +1,27 @@
+"use client"
+const { useState } = require("react")
+
+const Counter = () => {
+    const [contador, setContador] = useState(0);
+
+    const incrementarContador = () => {
+        setContador(contador + 1);
+    }
+
+    const decrementarContador = () => {
+        if (contador > 0) {
+            setContador(contador - 1);
+        }
+    }
+
+    return (
+        <div className="flex mt-10">
+            <h1 className="font-sans text-3xl">Contador</h1>
+            <button className="rounded-full bg-sky-500 px-5 py-2 text-sm leading-5 font-semibold text-white hover:bg-sky-700" onClick={decrementarContador}> - </button>
+            <button className="rounded-full bg-sky-500 px-5 py-2 text-sm leading-5 font-semibold text-white hover:bg-sky-700">Contador: {contador}</button>
+            <button className="rounded-full bg-sky-500 px-5 py-2 text-sm leading-5 font-semibold text-white hover:bg-sky-700" onClick={incrementarContador}> + </button>
+        </div>
+    )
+}
+
+export default Counter
---
+++ app/Clase1/Curso.js
@@ -0,0 +1,7 @@
+const Curso = () => {
+    return (
+        <h3 className="font-mono text-xl">Curso de Next JS</h3>
+    )
+}
+
+export default Curso
