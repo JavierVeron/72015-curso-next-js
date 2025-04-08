366204fb5eee1875eb6924f916e86114b131815a
Javier Veron <javier.veron@gmail.com>
2025-04-08 09:51:28 -0300

    Clase 7

--- app/firebase/useFireStore.js
+++ app/firebase/useFireStore.js
@@ -1,5 +1,5 @@
 const useFireStore = async (id) => {
-    const urlBase = "http://localhost:3000/api/";
+    const urlBase = import.meta.env.URL_BASE + "/api/";
     const url = urlBase + (id ? "producto/" + id : "productos");    
     const response = await fetch(url, {cache:"no-store"});
     const items = await response.json();
